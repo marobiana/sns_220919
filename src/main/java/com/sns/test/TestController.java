e4ee7973f53f25d2ba545cbca47083d03da1eadd
test <test@test.com>
2023-01-12 18:15:00 +0900

    초기 환경 세팅

---
+++ src/main/java/com/sns/test/TestController.java
@@ -0,0 +1,46 @@
+package com.sns.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.post.dao.PostDAO;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	private PostDAO postDAO;
+	
+	@ResponseBody
+	@GetMapping("/test1")
+	public String test1() {
+		return "테스트1";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test2")
+	public Map<String, Object> test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("test1", 111);
+		map.put("test2", 222);
+		map.put("test3", 333);
+		return map;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test4")
+	public List<Map<String, Object>> test4() {
+		return postDAO.selectPostListTest();
+	}
+}
